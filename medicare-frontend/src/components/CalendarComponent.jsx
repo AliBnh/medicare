e7f46754c88cb795b930d77176960afd6439a534
aliBnh <benhima150@gmail.com>
2024-05-27 14:39:05 +0100

    calendarUpdate

--- medicare-frontend/src/components/CalendarComponent.jsx
+++ medicare-frontend/src/components/CalendarComponent.jsx
@@ -4,18 +4,26 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 
 function CalendarComponent({ appointments }) {
   const events = appointments.map((rdv) => ({
-    title: rdv.patient_name,
+    title:
+      rdv.patient_name +
+      " - " +
+      new Intl.DateTimeFormat("en-GB", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      }).format(new Date(`1970-01-01T${rdv.time}Z`)),
     start: rdv.date,
     allDay: true,
   }));
 
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin]}
-      initialView="dayGridMonth"
-      events={events}
-      
-    />
+    <div className="calendar-container">
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={events}
+      />
+    </div>
   );
 }
 
